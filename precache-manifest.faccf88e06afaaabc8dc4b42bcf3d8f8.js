6305bfbd5edf7fde63239a7c0f94f2bcf2fcb1d6
ankitaargade <argade06ankita@gmail.com>
2020-08-29 16:30:56 +0530

    Updates

---
+++ precache-manifest.faccf88e06afaaabc8dc4b42bcf3d8f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d458d563092e425fe0b534aecfff21c4",
+    "url": "/meme-generator-react/index.html"
+  },
+  {
+    "revision": "36cae7d1a193f0d57a4d",
+    "url": "/meme-generator-react/static/css/main.24f356c7.chunk.css"
+  },
+  {
+    "revision": "7ba88a5a5b7ec23247e6",
+    "url": "/meme-generator-react/static/js/2.a87529c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator-react/static/js/2.a87529c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36cae7d1a193f0d57a4d",
+    "url": "/meme-generator-react/static/js/main.32a236eb.chunk.js"
+  },
+  {
+    "revision": "dddbcfa15ed028ec66db",
+    "url": "/meme-generator-react/static/js/runtime-main.d4033606.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meme-generator-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
